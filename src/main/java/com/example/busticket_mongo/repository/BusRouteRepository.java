aea01bac563ded8b1e73be6646584d984429b539
Myint Myat <playerone689@gmail.com>
2020-05-11 19:52:51 +0630

    queryDsl method

--- src/main/java/com/example/busticket_mongo/repository/BusRouteRepository.java
+++ src/main/java/com/example/busticket_mongo/repository/BusRouteRepository.java
@@ -2,12 +2,14 @@ package com.example.busticket_mongo.repository;
 
 import com.example.busticket_mongo.model.BusRoute;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BusRouteRepository extends MongoRepository<BusRoute,String> {
+public interface BusRouteRepository extends MongoRepository<BusRoute,String> ,
+        QuerydslPredicateExecutor<BusRoute> {
 
     List<BusRoute> findByBusNameContains(String busName);
 
--- src/main/java/com/example/busticket_mongo/resource/BusRouteController.java
+++ src/main/java/com/example/busticket_mongo/resource/BusRouteController.java
@@ -1,7 +1,12 @@
 package com.example.busticket_mongo.resource;
 
 import com.example.busticket_mongo.model.BusRoute;
+import com.example.busticket_mongo.model.BusStatus;
+import com.example.busticket_mongo.model.QBusRoute;
+import com.example.busticket_mongo.model.QRoutes;
 import com.example.busticket_mongo.repository.BusRouteRepository;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,5 +43,17 @@ public class BusRouteController {
     public Collection<BusRoute> byToCity(@PathVariable String toCity){
         return this.busRouteRepository.findByRoutesToCity(toCity);
     }
+
+    @GetMapping("/query/{busName}/{toCity}")
+    public Collection<BusRoute> byBusNameAndToCItyAndStatus(@PathVariable String busName,@PathVariable String toCity){
+        QBusRoute qBusRoute = new QBusRoute("query");
+        BooleanExpression busNamePredicate = qBusRoute.bus.name.eq(busName);
+         Predicate cityPredicate = qBusRoute.routes.toCity.eq(toCity);
+         Predicate busStatusPredicate = qBusRoute.bus.busStatus.eq(BusStatus.MODERATE);
+         Predicate customPredicate = busNamePredicate.and(cityPredicate).and(busStatusPredicate);
+
+         return (Collection<BusRoute>) this.busRouteRepository.findAll(customPredicate);
+    }
+
 
 }
